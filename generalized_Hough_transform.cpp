f763efe71f12cbc4666c3c13fbf17151e45db5e3
Navid Pourhadi <npourhadi1998@gmail.com>
2023-07-05 14:42:28 +0200

    Update generalized_Hough_transform.cpp
    saving the regions of plates in a new image.
    still has some problem in localizing them.

--- generalized_Hough_transform.cpp
+++ generalized_Hough_transform.cpp
@@ -7,9 +7,14 @@
 using namespace std;
 using namespace cv;
 
-Mat src, src_gray;
-Mat detected_edges;
-const char* window_name2 = "original image";
+
+bool insideCircle(int center_x, int center_y, int x, int y, int radius){
+    float distance = sqrt(pow(x - center_x , 2) + pow(y - center_y , 2));
+    if (distance < radius){
+        return true;
+    }
+    else return false;
+}
 
 bool hasIntersections(const vector<Vec3f>& circles, const Vec3f& circle)
 {
@@ -19,14 +24,14 @@ bool hasIntersections(const vector<Vec3f>& circles, const Vec3f& circle)
         if (otherCircle != circle)
         {
             float distance = sqrt(pow(circle[0] - otherCircle[0], 2) + pow(circle[1] - otherCircle[1], 2));
+            
             if (distance < (circle[2] + otherCircle[2]))
             {
                 intersections++;
             }
         }
     }
-    cout<<intersections<<endl;
-    if(intersections < 2){
+    if(intersections < 3){
         return false;
     }
     return true;
@@ -55,14 +60,14 @@ int main( int argc, char** argv )
     }
 
 
-
+    Mat src_gray;
     cvtColor( src, src_gray, COLOR_BGR2GRAY );
-    imshow( window_name2, src );
+    imshow( "original image", src );
     waitKey();
     blur( src_gray, src_gray, Size(7,7) );
 
     vector<Vec3f> circles;
-    HoughCircles(src_gray, circles, HOUGH_GRADIENT_ALT, 1,200, 0.7, 0.9, 150, 600);
+    HoughCircles(src_gray, circles, HOUGH_GRADIENT_ALT, 1,100, 0.7, 0.9, 150, 600);
     
     vector<Vec3f> filteredCircles;
     for (const auto& circle : circles)
@@ -71,39 +76,36 @@ int main( int argc, char** argv )
             filteredCircles.push_back(circle);
     }
     int j = 0;
+    Mat output_image = Mat::zeros(src.size(), src.type());
+
     for( size_t i = 0; i < filteredCircles.size(); i++ )
     {
         Vec3i c = filteredCircles[i];
         Point center = Point(c[0], c[1]);
         int radius = c[2];
 
-        int x = c[0] - c[2];
-        int y = c[1] - c[2];
-        int width = 2 * radius;
-        int height = 2 * radius;
-        
-        if(x < 2){
-            x = 2;    
-        }
-        else if(x + width > src.cols){
-            width = src.cols - x - 2;
-        }
-        if(y < 2){
-            y = 2;    
-        }
-        else if(y + height > src.rows){
-            height = src.rows - y - 2;
+        int left = c[0] - radius >= 0 ? radius - 1 : c[0] - 1;
+        int right = src.cols - c[0] >= radius ? radius - 1 : src.cols - c[0] - 1;
+        int up = c[1] - radius >= 0 ? radius - 1 : c[1] - 1;
+        int down = src.rows - c[1] >= radius ? radius - 1 : src.rows - c[1] - 1;
+
+        for(int row = c[1] - up; row< c[1] + down; row++){
+            for(int col=c[0] - left; col < c[0] + right; col++){
+                if (insideCircle(c[0],c[1],col,row, radius-10)){
+                    Vec3b pixel = src.at<Vec3b>(row,col);
+                    output_image.at<Vec3b>(row,col) = pixel;
+                }
+            }
         }
 
-        cv::Rect cropRect(x, y, width, height);
-        cv::Mat croppedImage = src(cropRect);
-        imwrite("../detection/"+to_string(j)+".jpg", croppedImage);
-
-        imshow("cropped part", croppedImage);
 
         j++;
-        waitKey();
     }
 
+    imwrite("../detection/"+to_string(j)+".jpg", output_image);
+
+    imshow("detected plate", output_image);
+    waitKey();
+    
    return 0;
 }
