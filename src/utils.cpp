e102b163fe406420c774a7f07d7e27920cb51337
matehrani <t.mohammadamin@gmail.com>
2023-07-18 16:20:36 +0200

    init branch

---
+++ src/utils.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+void mAP() {
+    // TODO
+}
+
+void mIoU() {
+    // TODO
+}
+
+int main() {
+    cout << "Please implement mIoU and mAP functions here." << endl;
+    return 0;
+}
