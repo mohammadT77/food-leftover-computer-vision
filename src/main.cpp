e1517ccd8459a160e175252b4fc9cf6ac87ba5ac
Navid Pourhadi <npourhadi1998@gmail.com>
2023-07-19 16:49:29 +0200

    update main.cpp
    change the obtained object region as circle for less intersection

--- src/main.cpp
+++ src/main.cpp
@@ -8,50 +8,40 @@ using namespace std;
 using namespace cv;
 
 
-// void segmentation_reduction(Mat& segmentation, int threshold){
+void GRAB_CUT(Mat& image) {
+    
+    // Define the bounding box for the foreground object
+    Rect boundingBox(1, 1, image.cols, image.rows);
 
-//     std::vector<std::vector<Point>> contours;
-//     std::vector<Vec4i> hierarchy;
-//     findContours(segmentation, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
+    // Perform bounds checking for the ROI
+    boundingBox.x = max(0, boundingBox.x);
+    boundingBox.y = max(0, boundingBox.y);
+    boundingBox.width = min(boundingBox.width, image.cols - boundingBox.x);
+    boundingBox.height = min(boundingBox.height, image.rows - boundingBox.y);
 
-//     Mat contour_image = Mat::zeros(segmentation.size(), CV_8UC3);
-//     for (size_t i = 0; i < contours.size(); i++) {
-//     drawContours(contour_image, contours, static_cast<int>(i), Scalar(0, 0, 255), 1);
-//     }
+    // Create a mask to indicate the foreground and background regions
+    Mat mask(image.size(), CV_8UC1, Scalar(GC_BGD));
+    mask(boundingBox).setTo(Scalar(GC_PR_FGD));  // Set the bounding box region as probable foreground
 
-//     // Show the contour image
-//     imshow("segmentation", segmentation);
-//     imshow("Contours", contour_image);
-//     waitKey(0);
+    // Run the GrabCut algorithm
+    Mat result;
+    grabCut(image, mask, boundingBox, Mat(), Mat(), 10, GC_INIT_WITH_RECT);
 
-//     std::vector<std::vector<Point>> filteredContours;
+    // Extract the foreground region based on the final mask
+    Mat foregroundMask = (mask == GC_PR_FGD) | (mask == GC_FGD);
+    Mat foregroundImage;
+    image.copyTo(foregroundImage, foregroundMask);
 
-//     for (size_t i = 0; i < contours.size(); i++) {
-//         double area = cv::contourArea(contours[i]);
+    // Display the original image, mask, and extracted foreground
+    namedWindow("Original Image", WINDOW_NORMAL);
+    namedWindow("Mask", WINDOW_NORMAL);
+    namedWindow("Foreground", WINDOW_NORMAL);
+    // imshow("Original Image", image);
+    imshow("Mask", mask * 64);  // Scale the mask for better visualization
+    imshow("Foreground", foregroundImage);
+    waitKey(0);
 
-//         if (area < threshold) {
-//             for (const auto& point : contours[i]) {
-//                 segmentation.at<uchar>(point) = 0;
-//             }
-//             continue;
-//         }
-//         else{
-//             filteredContours.push_back(contours[i]);
-//         }
-//     }
-
-//     // Draw contours
-//     Mat contour_image2 = Mat::zeros(segmentation.size(), CV_8UC3);
-//     for (size_t i = 0; i < filteredContours.size(); i++) {
-//     drawContours(contour_image2, filteredContours, static_cast<int>(i), Scalar(0, 0, 255), 1);
-//     }
-
-//     // Show the contour image
-//     imshow("segmentation", segmentation);
-//     imshow("Contours", contour_image2);
-//     waitKey(0);
-
-// }
+}
 
 bool insideCircle(int center_x, int center_y, int x, int y, int radius){
     float distance = sqrt(pow(x - center_x , 2) + pow(y - center_y , 2));
@@ -104,21 +94,18 @@ void extract_plates(Mat& src, Mat& object, Mat& non_object){
         Point center = Point(c[0], c[1]);
         int radius = c[2];
 
-        int left = c[0] - radius >= 0 ? radius - 1 : c[0] - 1;
-        int right = src.cols - c[0] >= radius ? radius - 1 : src.cols - c[0] - 1;
-        int up = c[1] - radius >= 0 ? radius - 1 : c[1] - 1;
-        int down = src.rows - c[1] >= radius ? radius - 1 : src.rows - c[1] - 1;
+        int left = c[0] - radius >= 0 ? radius : c[0];
+        int right = src.cols - c[0] >= radius ? radius : src.cols - c[0];
+        int up = c[1] - radius >= 0 ? radius : c[1];
+        int down = src.rows - c[1] >= radius ? radius : src.rows - c[1];
 
         for(int row = c[1] - up; row< c[1] + down; row++){
             for(int col=c[0] - left; col < c[0] + right; col++){
-                if (insideCircle(c[0],c[1],col,row, radius-10)){
-                    Vec3b pixel = src.at<Vec3b>(row,col);
-                    object.at<Vec3b>(row,col) = pixel;
+                if (insideCircle(c[0],c[1],col,row, radius-30)){
+                    object.at<Vec3b>(row,col) = src.at<Vec3b>(row,col);;
                 }
-                if (insideCircle(c[0],c[1],col,row, radius + 5)){
-                    non_object.at<Vec3b>(row,col)[0] = 0;
-                    non_object.at<Vec3b>(row,col)[1] = 0;
-                    non_object.at<Vec3b>(row,col)[2] = 0;
+                if (insideCircle(c[0],c[1],col,row, radius)){
+                    non_object.at<Vec3b>(row,col) = Vec3b(0,0,0);
                 }
             }
         }
@@ -140,25 +127,20 @@ void extract_objects(Mat& object, Mat& non_object){
             int distance1 = abs(Blue - Green); 
             int distance2 = abs(Green - Red);
             int distance3 = abs(Red - Blue);
-            if (((Blue - Red > 50) && (Blue - Green > 50)) || ((Red > Blue) && (Green > Blue) && (distance2 <= 21)) ){
+            if (((Blue - Red > 20) && (Blue - Green > 20)) || ((Red > Blue) && (Green > Blue) && (distance2 <= 21)) ){
                 continue;
             }
-            else if (abs(distance1 - distance2) > 35 || abs(distance2 - distance3) > 35 || abs(distance3 - distance1) > 35){
+            else if (abs(distance1 - distance2) > 35 || abs(distance2 - distance3) > 37 || abs(distance3 - distance1) > 35){
                 segmentation.at<uchar>(j,i) = 255;    
             }
                         
         }
     }
 
+    Mat elementDilation = getStructuringElement(MORPH_ELLIPSE, Size(13,13));
+    // Mat elementDilation2 = getStructuringElement(MORPH_ELLIPSE, Size(13,13));
 
-    int dilationSize = 6;
-
-    int erosionSize1 = 2;
-
-    Mat elementDilation = getStructuringElement(MORPH_RECT, Size(2 * dilationSize + 1, 2 * dilationSize + 1), Point(dilationSize, dilationSize));
-    Mat elementDilation2 = getStructuringElement(MORPH_RECT, Size(5,5), Point(2,2));
-
-    Mat elementErosion = getStructuringElement(MORPH_RECT, Size(3,3), Point(1,1));
+    Mat elementErosion = getStructuringElement(MORPH_RECT, Size(3,3));
 
     imshow("segmentation", segmentation);
     waitKey();
@@ -166,7 +148,7 @@ void extract_objects(Mat& object, Mat& non_object){
     erode(segmentation, segmentation, elementErosion);
     dilate(segmentation, segmentation, elementDilation);
     dilate(segmentation, segmentation, elementDilation);
-    dilate(segmentation, segmentation, elementDilation2);
+    dilate(segmentation, segmentation, elementDilation);
 
 
     // Find contours
@@ -177,58 +159,81 @@ void extract_objects(Mat& object, Mat& non_object){
     std::vector<std::vector<Point>> filteredContours;
 
     for (size_t i = 0; i < contours.size(); i++) {
-    double area = cv::contourArea(contours[i]);
+        double area = cv::contourArea(contours[i]);
 
-    if (area < 4500) {
-        continue;
-    }
-
-    bool isInsideAnotherContour = false;
-
-    // Iterate over the hierarchy to find if the contour is inside another contour
-    for (int j = 0; j >= 0; j = hierarchy[j][0]) {
-        if (j != static_cast<int>(i) && pointPolygonTest(contours[j], contours[i][0], false) >= 0) {
-            // Contour i is inside another contour
-            isInsideAnotherContour = true;
-            break;
+        if (area < 4500) {
+            continue;
         }
-    }
 
-    if (!isInsideAnotherContour) {
-        // Contour i is not inside any other contour, add it to filteredContours
-        filteredContours.push_back(contours[i]);
-    }
-    }
+        bool isInsideAnotherContour = false;
 
-
-    for (size_t i = 0; i < filteredContours.size(); i++) {
-        Rect boundingRect = cv::boundingRect(filteredContours[i]);
-        // object_regions.push_back(boundingRect);
-        for(int col = 0; col < boundingRect.width; col++){
-            for(int row = 0; row < boundingRect.height; row++){
-                if(object.at<Vec3b>(boundingRect.y + row, boundingRect.x + col) == Vec3b(0, 0, 0)){
-                    object.at<Vec3b>(boundingRect.y + row, boundingRect.x + col) = non_object.at<Vec3b>(boundingRect.y + row, boundingRect.x + col);
-
-                }
+        // Iterate over the hierarchy to find if the contour is inside another contour
+        for (int j = 0; j >= 0; j = hierarchy[j][0]) {
+            if (j != static_cast<int>(i) && pointPolygonTest(contours[j], contours[i][0], false) >= 0) {
+                // Contour i is inside another contour
+                isInsideAnotherContour = true;
+                break;
             }
         }
 
-        cv::Mat roi = non_object(boundingRect);
-        roi.setTo(cv::Scalar(0, 0, 0));        
+        if (!isInsideAnotherContour) {
+            // Contour i is not inside any other contour, add it to filteredContours
+            filteredContours.push_back(contours[i]);
+        }
     }
 
-    // Draw contours
     Mat contour_image = Mat::zeros(segmentation.size(), CV_8UC3);
     for (size_t i = 0; i < filteredContours.size(); i++) {
-    drawContours(contour_image, filteredContours, static_cast<int>(i), Scalar(0, 0, 255), 1);
-    drawContours(contour_image, contours, static_cast<int>(i), Scalar(0, 255, 255), 1);
-
+        drawContours(contour_image, filteredContours, static_cast<int>(i), Scalar(0, 0, 255), 1);
     }
-
+    namedWindow("Contours", WINDOW_NORMAL);
     // Show the contour image
     imshow("Contours", contour_image);
     waitKey(0);
 
+
+    for (size_t i = 0; i < filteredContours.size(); i++) {
+        // Find the minimum enclosing circle for the contour
+        cv::Point2f center;
+        float radius;
+        cv::minEnclosingCircle(filteredContours[i], center, radius);
+        radius = radius - 5;
+        int min_col = center.x - radius >= 0 ? center.x - radius : 0;
+        int max_col = center.x + radius <= contour_image.cols ? center.x + radius : contour_image.cols;
+        int min_row = center.y - radius >= 0 ? center.y - radius : 0;
+        int max_row = center.y + radius <= contour_image.rows ? center.y + radius : contour_image.rows;
+
+       for(int col = min_col; col <= max_col; col++) {
+            for(int row = min_row; row <= max_row; row++) {
+                // Check if the pixel is inside the circle
+                if (cv::norm(cv::Point2f(col, row) - center) < radius) {
+                    if (object.at<Vec3b>(row, col) == Vec3b(0, 0, 0)) {
+                        object.at<Vec3b>(row, col) = non_object.at<Vec3b>(row, col);
+                        non_object.at<Vec3b>(row, col) = Vec3b(0,0,0);
+                    }
+                }
+                
+            }
+        }
+    }
+
+
+    // for (size_t i = 0; i < filteredContours.size(); i++) {
+    //     Rect boundingRect = cv::boundingRect(filteredContours[i]);
+    //     // object_regions.push_back(boundingRect);
+    //     for(int col = 0; col < boundingRect.width; col++){
+    //         for(int row = 0; row < boundingRect.height; row++){
+    //             if(object.at<Vec3b>(boundingRect.y + row, boundingRect.x + col) == Vec3b(0, 0, 0)){
+    //                 object.at<Vec3b>(boundingRect.y + row, boundingRect.x + col) = non_object.at<Vec3b>(boundingRect.y + row, boundingRect.x + col);
+
+    //             }
+    //         }
+    //     }
+
+    //     cv::Mat roi = non_object(boundingRect);
+    //     roi.setTo(cv::Scalar(0, 0, 0));        
+    // }
+    
 }
 
 void get_objects(Mat& objects, vector<cv::Rect>& regions){
@@ -282,6 +287,14 @@ int main( int argc, char** argv )
         return -1;
     }
 
+
+//   const int kNeighborhoodDiamter = 5;
+//   const int kSigmaColor = 1000;
+//   const int kSigmaSpace = 200;
+
+//   bilateralFilter(input_image_, filtered_image_, kNeighborhoodDiamter, kSigmaColor, kSigmaSpace);
+
+
 // applying laplacian filter
     std::vector<cv::Mat> channels;
     cv::split(src, channels);
@@ -296,6 +309,8 @@ int main( int argc, char** argv )
     cv::merge(channels, sharpenedImage);
     cv::Mat image_sharp = src - sharpenedImage;
 
+    // GRAB_CUT(image_sharp);
+
 // variable declaration of images 
     Mat objects = Mat::zeros(image_sharp.size(), CV_8UC3);
     Mat non_objects = image_sharp.clone();    
@@ -306,14 +321,23 @@ int main( int argc, char** argv )
     extract_objects(objects, non_objects);
 
     get_objects(objects, object_regions);
-    for(const auto& region : object_regions){
-        cout<<region<<endl;
-    }
+    
 // displaying objects and non_objects images
     imshow("detected", objects);
     imshow("non detected", non_objects);    
     waitKey();
 
+    for(const auto& region : object_regions){
+
+        Mat detected_object = image_sharp(region);
+        detected_object.convertTo(detected_object, CV_8UC3);
+
+        // GRAB_CUT(detected_object);
+
+        imshow("region",  detected_object);
+        waitKey();
+
+    }
 
     return 0;
 }
